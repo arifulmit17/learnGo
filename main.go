31ed9114842376298b2120ea054812c8abc18009
Ariful <arifulmit17@gmail.com>
2026-06-24 23:27:41 +0600

    added  another variable declaration

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("Welcome to Go !")
 	// var name string = "Howard"
-	name := "Shawn"
+	// name := "Shawn"
+	var name = "James"
 	fmt.Printf("Hello %s", name)
 }
